526e8c0f2378f8ddeb8c34c9c73f5c71b6d97023
Nikita Ščipanovs <Nikita.Scipanovs@edu.rtu.lv>
2023-02-20 01:41:20 +0200

    test1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # python3
-#asdsadasd
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -11,19 +11,35 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next,i))
+            # print(opening_brackets_stack)
+        
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if opening_brackets_stack:
+                if (are_matching(opening_brackets_stack[len(opening_brackets_stack)-1].char,next)):
+                    opening_brackets_stack.pop(len(opening_brackets_stack)-1)
+                    # print(opening_brackets_stack)
+                else:
+                    print(i+1)
+                    break
+            else:
+                # print("net otkritoj")
+                print()
+                
             pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
+    
     # Printing answer, write your code here
 
 
